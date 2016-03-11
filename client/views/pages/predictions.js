fa1ca89f49048fcc1e8576edd18a8fe59569ad17
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-11 22:03:29 +0100

    saving predictions

--- client/views/pages/predictions.js
+++ client/views/pages/predictions.js
@@ -1,14 +1,52 @@
 Template.predictions.helpers({
-    getPredictions: function () {
+    getQuestions: function () {
         return questions.find({});
+    },
+    getPredictions: function () {
+        return predictions.find({});
     }
 });
 
 Template.predictions.events({
     "focusout": function (event) {
         if (event.target.value) {
-            console.info(event.target.id + ' ' + event.target.value + ' ' + event.joke);
-            console.info(event);
+            inputUpdateScore(event.target.id, event.target.name, event.target.value);
+        }
+    },
+    "keyup": function (event) {
+        if (event.which == 13 && event.target.value) {
+            inputUpdateScore(event.target.id, event.target.name, event.target.value);
         }
     }
 });
+
+var inputUpdateScore = function (id, name, value) {
+    var question = questions.findOne({
+        _id: id
+    });
+    if (question) {
+        console.info(id + ' ' + name + ' ' + value);
+        var prediction = predictions.findOne({
+            _id: id
+        });
+
+        var obj = {};
+        obj[name] = value;
+        obj['date'] = new Date();
+        if (prediction) {
+            predictions.update({
+                _id: id
+            }, {
+                $set: obj
+
+            });
+        } else {
+            obj['_id'] = id;
+            predictions.insert(
+                obj
+            );
+        }
+
+        console.log("saved!");
+    }
+}
--- common/collections.js
+++ common/collections.js
@@ -1,2 +1,12 @@
 participants = new Meteor.Collection('participants');
 questions = new Meteor.Collection('questions');
+predictions = new Meteor.Collection('predictions');
+
+predictions.allow({
+    update: function (userId, doc) {
+        return true;
+    },
+    insert: function (userId, doc) {
+        return true;
+    }
+});
