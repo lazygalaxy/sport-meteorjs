b555aef4120d44f1ee2b3b1720eadbe8e03ab421
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-13 06:18:01 +0100

    each user can now predict his own scores

--- client/views/pages/predictions.js
+++ client/views/pages/predictions.js
@@ -1,18 +1,22 @@
 Template.predictions.helpers({
     getQuestions: function () {
-        var predictionsMap = predictions.find({}).fetch().reduce(function (map, obj) {
-            map[obj._id] = obj;
-            return map;
-        }, {});
-
         var theQuestions = questions.find({}).fetch();
-        theQuestions.forEach(function (line) {
-            if (line._id in predictionsMap) {
-                line.home_score = predictionsMap[line._id].home_score;
-                line.away_score = predictionsMap[line._id].away_score;
-            }
-        });
+        if (Meteor.user()) {
+            var predictionsMap = predictions.find({
+                user_id: Meteor.user()._id
+            }).fetch().reduce(function (map, obj) {
+                map[obj.question_id] = obj;
+                return map;
+            }, {});
 
+            theQuestions.forEach(function (question) {
+                if (question._id in predictionsMap) {
+                    question.home_score = predictionsMap[question._id].home_score;
+                    question.away_score = predictionsMap[question._id].away_score;
+                }
+            });
+        }
+        console.log(theQuestions);
         return theQuestions;
     }
 });
@@ -32,6 +36,6 @@ Template.predictions.events({
 
 var inputUpdateScore = function (id, name, value) {
     Meteor.call('upsertPrediction', id, name, value, function () {
-        console.info('done!!!');
+        console.info('update the GUI here!!!');
     });
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 //methods
 Meteor.methods({
-    'upsertPrediction': function (id, name, value) {
-        var obj = {};
-        obj[name] = value;
-        obj['date'] = new Date();
-        predictions.upsert({
-            _id: id
-        }, {
-            $set: obj
-        });
+    //upserts a prediction based on the id of a question and the name and value provided
+    'upsertPrediction': function (question_id, name, value) {
+        if (Meteor.user()) {
+            var obj = {};
+            obj[name] = value;
+            obj['date'] = new Date();
+            obj['question_id'] = question_id;
+            obj['user_id'] = Meteor.user()._id;
+            predictions.upsert({
+                _id: question_id + '_' + Meteor.user()._id
+            }, {
+                $set: obj
+            });
+        }
     }
 });
