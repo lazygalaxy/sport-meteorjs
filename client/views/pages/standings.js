87e05efb97769c8e23b8fd730f58d1881a05132e
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-17 07:28:01 +0100

    scoring mechanism in place

--- client/views/pages/standings.js
+++ client/views/pages/standings.js
@@ -6,7 +6,7 @@ Template.standings.helpers({
         var predictionMap = Predictions.find({
             competitionId: competitionId
         }).fetch().reduce(function (map, obj) {
-            if (~map[obj.userId]) {
+            if (!map[obj.userId]) {
                 map[obj.userId] = {}
             }
             map[obj.userId][obj._id] = obj;
@@ -16,27 +16,32 @@ Template.standings.helpers({
         var resultMap = Results.find({
             competitionId: competitionId
         }).fetch().reduce(function (map, obj) {
-            if (~map[obj.userId]) {
-                map[obj.userId] = {}
-            }
-            map[obj.userId][obj._id] = obj;
+            //TODO: consider the highest weighted here if multiple exists
+            map[obj.itemId] = obj;
             return map;
         }, {});
 
         var users = [];
-        Object.keys(predictionMap).forEach(function (userKey) {
+        Object.keys(predictionMap).forEach(function (userId) {
             var user = CustomUsers.findOne({
-                _id: userKey
+                _id: userId
             });
-            user['points'] = 0;
+            user.points = 0;
 
-            Object.keys(predictionMap[userKey]).forEach(function (predictionKey) {
-                var prediction = predictionMap[userKey][predictionKey];
-                if (resultMap[userKey]) {
-                    var result = resultMap[userKey][prediction.itemKey];
+            //console.info("calculating user " + user.username);
+            Object.keys(predictionMap[userId]).forEach(function (predictionId) {
+                var prediction = predictionMap[userId][predictionId];
+                //console.info("calculating prediction " + JSON.stringify(prediction));
+                if (resultMap[prediction.itemId]) {
+                    var result = resultMap[prediction.itemId];
                     if (result && prediction) {
+                        //console.log("calculating " + prediction.itemId);
                         if (result.homeScore == prediction.homeScore && result.awayScore == prediction.awayScore) {
-                            user['points'] += 3;
+                            user.points += 3;
+                        } else if ((result.homeScore - result.awayScore) == (prediction.homeScore - prediction.awayScore)) {
+                            user.points += 2;
+                        } else if ((result.homeScore > result.awayScore && prediction.awayScore > prediction.awayScore) || (result.homeScore < result.awayScore && prediction.awayScore < prediction.awayScore)) {
+                            user.points += 1;
                         }
                     }
                 }
