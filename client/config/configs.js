9a15630ab5c0ba8dea48a82fa8a579e59215f74f
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-31 21:05:31 +0200

    removed accounts-ui

--- client/config/configs.js
+++ client/config/configs.js
@@ -3,8 +3,3 @@ CustomUsers = new Mongo.Collection('customusers');
 
 //subscriptions
 Meteor.subscribe("customusers");
-
-//accounts
-Accounts.ui.config({
-    passwordSignupFields: "USERNAME_AND_EMAIL"
-});
--- client/views/common/navigation.js
+++ client/views/common/navigation.js
@@ -2,3 +2,11 @@ Template.navigation.rendered = function () {
     // Initialize metisMenu
     $('#side-menu').metisMenu();
 };
+
+Template.navigation.events({
+    'click .logout': function (event) {
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('login');
+    }
+});
