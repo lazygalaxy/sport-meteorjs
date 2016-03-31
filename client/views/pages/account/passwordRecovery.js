2c3a26c714bcb011d55a1ed7d2840f8eec515166
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-31 21:47:42 +0200

    change password modal in place

---
+++ client/views/pages/account/passwordRecovery.js
@@ -0,0 +1,16 @@
+Template.passwordRecovery.events({
+    'submit form': function (event) {
+        event.preventDefault();
+        var email = $('[name=email]').val();
+
+        Accounts.forgotPassword({
+            email: email
+        }, function (error) {
+            if (error) {
+                toastr.error(error.reason, 'Password Recovery')
+            } else {
+                toastr.success('A new password has been emailed to you.', 'Password Recovery')
+            }
+        });
+    }
+});
