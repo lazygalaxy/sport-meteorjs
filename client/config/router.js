5bb79adf4b5be34ebfef06067e27332a3868afe0
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-04-02 09:12:13 +0200

    prevent selected competitions and groups being reset

--- client/config/router.js
+++ client/config/router.js
@@ -62,14 +62,9 @@ Router.route('/standings', {
         return [Meteor.subscribe("customusers")];
     },
     action: function () {
-        //TODO: not entirely sure this is the best way to handle this
-        if (getCurrentUser()) {
-            var groupLength = getCurrentUser().groups.length;
-            //TODO: should not be hardcoded to EURO2016
-            Session.set('selectedCompetition', 'EURO2016');
-            Session.set('selectedGroup', getCurrentUser().groups[groupLength - 1]);
-            this.render('standings');
-        }
+        setCompetition(false);
+        setGroup(false);
+        this.render('standings');
     }
 });
 
@@ -88,13 +83,36 @@ Router.route('/userAdmin', {
         return Meteor.subscribe("customusers");
     },
     action: function () {
-        //TODO: not entirely sure this is the best way to handle this
-        if (getCurrentUser()) {
-            var groupLength = getCurrentUser().adminGroups.length;
-            //TODO: should not be hardcoded to EURO2016
-            Session.set('selectedCompetition', 'EURO2016');
-            Session.set('selectedGroup', getCurrentUser().adminGroups[groupLength - 1]);
-            this.render('userAdmin');
-        }
+        setCompetition(false);
+        setGroup(true);
+        this.render('userAdmin');
     }
 });
+
+var setCompetition = function (checkAdmin) {
+    //ensure that a competition is set
+    if (!Session.get('selectedCompetition')) {
+        //TODO: should not be hardcoded to EURO2016
+        Session.set('selectedCompetition', 'EURO2016');
+    }
+
+    //if it is required ensure it is an admin group
+    if (checkAdmin && getCurrentUser().adminCompetitions.indexOf(Session.get('selectedCompetition')) == -1) {
+        var competitionLength = getCurrentUser().adminCompetitions.length;
+        Session.set('selectedCompetition', getCurrentUser().adminCompetitions[0]);
+    }
+}
+
+var setGroup = function (checkAdmin) {
+    //ensure that a group is set
+    if (!Session.get('selectedGroup')) {
+        var groupLength = getCurrentUser().groups.length;
+        Session.set('selectedGroup', getCurrentUser().groups[groupLength - 1]);
+    }
+
+    //if it is required ensure it is an admin group
+    if (checkAdmin && getCurrentUser().adminGroups.indexOf(Session.get('selectedGroup')) == -1) {
+        var groupLength = getCurrentUser().adminGroups.length;
+        Session.set('selectedGroup', getCurrentUser().adminGroups[groupLength - 1]);
+    }
+}
