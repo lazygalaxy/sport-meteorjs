1915cdd76489502e5fecfe498dfeab274d71de27
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-04-10 22:44:34 +0200

    copy to clipboard implemented

--- client/pages/admin/userAdmin.js
+++ client/pages/admin/userAdmin.js
@@ -4,47 +4,28 @@ Template.userRow.events({
     }
 });
 
+var getGroupUsers = function () {
+    return CustomUsers.find({
+        groups: Session.get('selectedGroup')._id
+    }).fetch();
+}
+
 Template.userAdmin.helpers({
     getGroupUsers: function () {
-        return CustomUsers.find({
-            groups: Session.get('selectedGroup')._id
+        return getGroupUsers();
+    },
+    getClipboardEmails: function () {
+        var emails = "";
+
+        getGroupUsers().forEach(function (user) {
+            emails += user.emails[0].address + ';';
         });
+        console.info(emails);
+        return emails;
     }
 });
 
 Template.userAdmin.rendered = function () {
     // Initialize dataTables
-    $('.dataTables-users').DataTable({
-        dom: '<"html5buttons"B>lTfgitp',
-        buttons: [
-            {
-                extend: 'copy'
-            },
-            {
-                extend: 'csv'
-            },
-            {
-                extend: 'excel',
-                title: 'ExampleFile'
-            },
-            {
-                extend: 'pdf',
-                title: 'ExampleFile'
-            },
-
-            {
-                extend: 'print',
-                customize: function (win) {
-                    $(win.document.body).addClass('white-bg');
-                    $(win.document.body).css('font-size', '10px');
-
-                    $(win.document.body).find('table')
-                        .addClass('compact')
-                        .css('font-size', 'inherit');
-                }
-            }
-        ]
-
-    });
-
+    $('.dataTables-adminusers').DataTable();
 };
